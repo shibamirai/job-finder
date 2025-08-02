e21a2ef8e4ae9243fa9c2f96b58d33eb3f954c81
shibamirai <t_shiba@miraino-katachi.co.jp>
2025-08-02 16:19:40 +0900

    ロゴのパスを変更

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,6 +1,6 @@
 <template>
     <img
-        src="https://cdn.goope.jp/66978/191105144609-5dc10ca18a32b.png"
+        src="https://miraino-katachi.co.jp/wp-content/uploads/2023/09/logo.png"
         alt="大阪本町のプログラミング IT特化就労移行支援事業所　未来のかたち"
     >
 </template>
